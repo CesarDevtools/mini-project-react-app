1a8e918d373066d2ec594a86bb2604ad9002944a
Cesar <cessardevs@gmail.com>
2026-03-11 10:25:37 +0000

    Fixed css selectors

--- recipe-book-project/src/components/Navbar.jsx
+++ recipe-book-project/src/components/Navbar.jsx
@@ -2,19 +2,17 @@ import "./Navbar.css";
 
 function Navbar() {
   return (
-    <>
-      <div className="navbar-container">
-        <h1 className="logo-mymeals">
-          <span className="logo-my">My</span>
-          <span className="logo-meals">Meals</span>
-        </h1>
+    <div className="navbar-container">
+      <h1 className="logo-mymeals">
+        <span className="logo-my">My</span>
+        <span className="logo-meals">Meals</span>
+      </h1>
 
-        <div className="menu">
-          <a href="">Home Page</a>
-          <a href="">About Page</a>
-        </div>
+      <div className="menu">
+        <a href="">Home Page</a>
+        <a href="">About Page</a>
       </div>
-    </>
+    </div>
   );
 }
 
